a71ce15a98116e3af0f2e33509c44c13cb7864e9
Gamza222 <turpurture@mail.ru>
2022-06-03 22:02:35 +0300

    Updates

---
+++ component---src-pages-bcomponets-ui-ie-circle-ie-circle-jsx-836cc5f81337445dabe3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkatlas_website=self.webpackChunkatlas_website||[]).push([[4918],{2910:function(e,t,i){i.r(t),i.d(t,{default:function(){return l}});var n=i(7294);function l(e){return n.createElement("div",{className:"IECircle-module--circle--MvIiC",style:{width:e.Width+"px",minWidth:"116px",maxWidth:"190px",height:e.Width+"px",minHeight:"116px",maxHeight:"190px"}},n.createElement("h4",null,e.Number,n.createElement("span",null,"Kg CO2")))}}}]);
+//# sourceMappingURL=component---src-pages-bcomponets-ui-ie-circle-ie-circle-jsx-836cc5f81337445dabe3.js.map
